f963327e3cd238273cbdc11abff604e314c6c75d
Maksym Trykoz <max3koz@gmail.com>
2026-05-29 11:15:48 +0200

    Update project after review and reopen this course

--- func.c
+++ func.c
@@ -4,7 +4,10 @@
 /*function definition*/
 void printfHelloMake(void) {
 
-    printf("Hello from \"func.c\" file!\n");
+    printf("\n");
+    printf("It is my first experience with C language )))\n");
+    printf("\n");
+    printf("Hello from func.c file!\n");
 
     return;
 }
